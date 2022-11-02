0fb6b34aa698ea2e26b21abb510942e920451cfc
Eshaan Aggarwal <eshaanagg@gmail.com>
2022-11-02 12:21:33 +0530

    Add files

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fresher's Survival Bot
+
+### How to run locally
+1. Setup the repo and install deno locally following the grammY documentation.
+2. Run `deno run --allow-net --allow-env bot.ts`
+3. Go to `@FresherSurvivalBot` on telegram to access the bot.
+4. Send `/start` command.
+
+### How to contribute
+1. Change the constants like `lectureHalls` and `hostels` to contain more information in the form of fancier markdown.
+2. Define a new constant, and then add the detail about that constant in the `commands` array.
---
+++ bot.ts
@@ -0,0 +1,60 @@
+import { Bot } from "https://deno.land/x/grammy@v1.11.2/mod.ts";
+import { InlineKeyboard } from "https://deno.land/x/grammy@v1.11.2/mod.ts";
+
+const lectureHalls = `
+Here are the halls on our campus:
+[LT 1.x](https://goo.gl/maps/RANXpqoEv7jy4KCP6)
+[LT 2.x](https://goo.gl/maps/gSNeUjT4S4Wu9bmr6)
+[LT 3.x](https://goo.gl/maps/GWSKbvzQ9y4Utn2s5)
+`;
+
+const hostels = `
+Here are the boy hostels on our campus:
+[Aryabhatta](https://goo\.gl/maps/6Ka5C6ykMSpAz73t5)
+[DG - 2](https://goo\.gl/maps/qgQCdYfT8iJF79ZK6)
+[Morvi](https://goo\.gl/maps/R31Zg6a5WN41g3vF7)
+`;
+
+
+const bot = new Bot(
+  "5462906162:AAEg7BD-wh7MgxTTkgZnFmjsMYCUFSLlJOc"
+);
+const commands = [
+  {
+    text: "Can't find my LT. Welpp!",
+    cb : "LT",
+    data: lectureHalls,
+  },
+  {
+    text: "Ugh, which hostel was that again?",
+    cb : "Hostel",
+    data: hostels,
+  },
+];
+
+const keyboard = new InlineKeyboard();
+commands.forEach((command) => {
+  keyboard.text(command.text, command.cb);
+  keyboard.row();
+})
+
+bot.on("callback_query:data", async (ctx) => {
+  let data = ctx.callbackQuery?.data;
+  commands.forEach(async (command) => {
+    if (command.cb == data) {
+      await ctx.answerCallbackQuery("Fetching data....");
+      await ctx.api.sendMessage(ctx.msg?.chat?.id, command.data, {
+        parse_mode: "Markdown"
+      });
+    }
+  })
+})
+
+bot.command("start", async (ctx) =>
+  await ctx.reply("Welcome! The bot is up and running. \nSend /commands to see all the available commands.")
+);
+bot.command("commands", async (ctx) => 
+  await ctx.reply("Here are the available commands: ", {reply_markup:keyboard})
+)
+
+bot.start();
